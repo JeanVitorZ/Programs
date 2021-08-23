ccea377ba9844a426d39d81273ac0425372600a6
JEAN VITOR ZIMMERMANN <jean.zimmermann@alunos.sc.senac.br>
2021-08-23 19:47:28 -0300

    Teste

---
+++ JavaApplication9/src/javaapplication9/NewClass.java
@@ -0,0 +1,32 @@
+
+package javaapplication9;
+
+ import java.io.*;
+
+public class NewClass implements Serializable {
+    
+    public String name;
+    public int idade;
+/*
+    public NewClass(String name, int idade) {
+        this.name = name;
+        this.idade = idade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }*/
+    
+}
