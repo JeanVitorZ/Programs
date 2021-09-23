b22ab10473868d2a98055dc5fa4645738ca7cad6
JEAN VITOR ZIMMERMANN <jean.zimmermann@alunos.sc.senac.br>
2021-09-22 21:45:34 -0300

    Programa Concluido

--- CadastroDeInfor/src/cadastrodeinfor/NewJFrame.java
+++ CadastroDeInfor/src/cadastrodeinfor/NewJFrame.java
@@ -12,9 +12,6 @@ import javax.swing.*;
  */
 public class NewJFrame extends javax.swing.JFrame {
 
-    /**
-     * Creates new form NewJFrame
-     */
     public NewJFrame() {
         initComponents();
         for (int i =1900; i < 2021; i++) {
@@ -55,56 +52,56 @@ public class NewJFrame extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel1.setText("Nome:");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 90, 62, 25));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 70, 62, 25));
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel2.setText("ID:");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, 62, 25));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 110, 62, 25));
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel3.setText("Endereço:");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 250, 100, 25));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 230, 100, 25));
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel4.setText("Data de Nascimento:");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 160, 180, 40));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 140, 180, 40));
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel5.setText("Contato:");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 210, 90, 25));
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 190, 90, 25));
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel6.setText("Departamento:");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 330, -1, 30));
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 310, -1, 30));
 
         jLabel7.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel7.setText("Genero:");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 290, 62, 25));
-        getContentPane().add(ID, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 130, 470, 30));
-        getContentPane().add(Nome, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 90, 470, 30));
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 270, 62, 25));
+        getContentPane().add(ID, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 110, 470, 30));
+        getContentPane().add(Nome, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 70, 470, 30));
 
         AnoA.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1900", "1901", "1902", "1903", "1904", "1905", "1906", "1907", "1908", "1909", "1910", "1911", "1912", "1913", "1914", "1915", "1916", "1917", "1918", "1919", "1920", "1921", "1922", "1923", "1924", "1925", "1926", "1927", "1928", "1929", "1930", "1931", "1932", "1933", "1934", "1935", "1936", "1937", "1938", "1939", "1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947", "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021" }));
-        getContentPane().add(AnoA, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 170, -1, -1));
+        getContentPane().add(AnoA, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 150, -1, -1));
 
         DataA.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
-        getContentPane().add(DataA, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 170, 80, -1));
+        getContentPane().add(DataA, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 150, 80, -1));
 
         MesA.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Set", "Out", "Nov", "Dez" }));
-        getContentPane().add(MesA, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 170, -1, -1));
+        getContentPane().add(MesA, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 150, -1, -1));
 
-        Dep.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "TI", "RH", "Financeiro", "Comercial", "Marketing", "SAC", "Logistica", "Recebimento", "Engenheiro", "Arquiteto", "Hacker" }));
-        getContentPane().add(Dep, new org.netbeans.lib.awtextra.AbsoluteConstraints(232, 340, 240, 30));
+        Dep.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "TI", "RH", "Financeiro", "Comercial", "Marketing", "SAC", "Logistica", "Recebimento", "Engenheiro", "Arquiteto", "Cyber Security" }));
+        getContentPane().add(Dep, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 320, 240, 30));
 
         Gen.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Masc.", "Fem." }));
-        getContentPane().add(Gen, new org.netbeans.lib.awtextra.AbsoluteConstraints(232, 290, 240, 30));
+        getContentPane().add(Gen, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 270, 240, 30));
 
         Ender.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 EnderActionPerformed(evt);
             }
         });
-        getContentPane().add(Ender, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 250, 470, 30));
-        getContentPane().add(Cont, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 212, 470, 30));
+        getContentPane().add(Ender, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 230, 470, 30));
+        getContentPane().add(Cont, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 190, 470, 30));
 
         Save.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         Save.setText("Save");
@@ -113,15 +110,16 @@ public class NewJFrame extends javax.swing.JFrame {
                 SaveActionPerformed(evt);
             }
         });
-        getContentPane().add(Save, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 420, 90, 40));
+        getContentPane().add(Save, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 400, 90, 40));
 
+        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jButton1.setText("Deletar Infos");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 420, 120, 40));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 400, 190, 40));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
